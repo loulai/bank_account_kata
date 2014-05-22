35ef6a16618dd7300132040f6cb46f33cfe86f1f
Louise <louiselai88@gmail.com>
2014-05-22 20:15:28 +0100

    wrapping primative objects attempt

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,11 +1,26 @@
 require 'bank_account'
+require 'amount'
 require 'spec_helper'
 
 describe 'BankAccount' do
 
 	before {@my_bank_account = BankAccount.new}
 
+
+
 	specify 'I can deposit 1 AUD' do
-		expect(@my_bank_account.deposit(1)).to eq 1
+		amount = Amount.new(1)
+		expect(@my_bank_account.deposit(amount)).to eq amount
+	end
+
+	specify 'I can deposit 5 AUD' do
+		amount = Amount.new(5)
+	 	expect(@my_bank_account.deposit(amount)).to eq amount
+	end
+
+	it "should return total balance if i de deposit twice" do
+		amount = Amount.new(1)
+		@my_bank_account.deposit(1)		
+		expect(@my_bank_account.deposit(1)).to eq 
 	end
 end
